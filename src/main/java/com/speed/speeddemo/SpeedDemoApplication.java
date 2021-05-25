512478ecc47d9c6e56d8e5a8fb21291c5d2ff383
Roger Diller <roger@flexrentalsolutions.com>
2021-05-25 16:04:22 -0400

    initial commit

---
+++ src/main/java/com/speed/speeddemo/SpeedDemoApplication.java
@@ -0,0 +1,13 @@
+package com.speed.speeddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/speed/speeddemo/SpeedDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speed.speeddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
