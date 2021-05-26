94ffabd06eb72c884b2937c6df30f504fb7ef9d3
Roger Diller <roger@flexrentalsolutions.com>
2021-05-26 11:56:08 -0400

    slowFibonacci

--- src/main/java/com/speed/speeddemo/SpeedController.java
+++ src/main/java/com/speed/speeddemo/SpeedController.java
@@ -1,14 +1,36 @@
 package com.speed.speeddemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpeedController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(SpeedController.class);
+
+    public SpeedController(){
+        long fib = 48;
+
+        LOG.info("Computing slow fibonacci: {}", fib);
+
+        long fibResult = slowFibonacci(fib);
+
+        LOG.info("slow fibonacci result: {}", fibResult);
+    }
+
     @GetMapping("/")
     public double fast(){
         return Math.random() * 100;
+    }
+
+    public static long slowFibonacci(long n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return slowFibonacci(n - 1) + slowFibonacci(n - 2);
+        }
     }
 
 }
